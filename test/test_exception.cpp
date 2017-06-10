12a68f768c3f2959901de01d0d751af13786dbbb
eldr <eldr@gmail.com>
2017-06-09 19:46:06 -0700

    add tests and test for exceptions

--- test/test_exception.cpp
+++ test/test_exception.cpp
@@ -6,8 +6,55 @@
  *
  */
 #include "../utility.h"
+#include <stdexcept>
+
+#include <iostream>
+#include <string>
+
+void erroneous (void)
+{
+    try
+    {
+        throw(std::runtime_error("oh nuts!"));
+    }
+    catch (std::runtime_error &e)
+    {
+        throw(std::runtime_error("oh noes!"));
+    }
+}
+
+int validate_int(std::string input)
+{
+    try
+    {
+        try
+        {
+            int number = std::stoi(input, nullptr, 10);
+        }
+        catch (std::invalid_argument &e)
+        {
+            throw(std::runtime_error("invalid argument caught"));
+        }
+    }
+    catch (std::runtime_error &e)
+    {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
 
 int main (void)
 {
+    if (validate_int(std::string("10")))
+    {
+        std::cout << "10 invalid\n";
+        return 1;
+    }
+    else if (validate_int(std::string("p")))
+    {
+        std::cout << "p invalid\n";
+        return 1;
+    }
 
 }
